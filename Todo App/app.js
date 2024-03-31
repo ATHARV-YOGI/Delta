f8539304377dddfceb87e2cc53f7bda5d99b4503
Atharv Yogi <atharvyogi123@gmail.com>
2024-03-31 19:42:41 +0530

    updated

--- Todo App/app.js
+++ Todo App/app.js
@@ -11,7 +11,32 @@ btn.addEventListener("click", function(){
     delbtn.classList.add("delete");
 
     item.appendChild(delbtn);
-
     ul.appendChild(item);
     inp.value="";
 });
+
+    ul.addEventListener("click", function(){
+        if(event.target.nodeName=="BUTTON")
+        {
+            let listItem=event.target.parentElement;
+            listItem.remove();
+            console.log("delete");
+        }
+    });
+
+
+
+
+
+
+// let delbtns = document.querySelectorAll(".delete");
+
+// for(delbtn of delbtns){
+
+//     delbtn.addEventListener("click", function(){
+//         let par = this.parentElement;
+//         console.log(par);
+//         par.remove();
+
+//     });  
+// }
